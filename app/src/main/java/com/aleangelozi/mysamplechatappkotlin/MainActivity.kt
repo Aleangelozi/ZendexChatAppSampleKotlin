e6dc5791290d481121921f2bac55ee3fd01ef096
aleangelozi <aleangelozi@gmail.com>
2021-04-23 08:56:12 -0300

    Setting visitor info.

--- app/src/main/java/com/aleangelozi/mysamplechatappkotlin/MainActivity.kt
+++ app/src/main/java/com/aleangelozi/mysamplechatappkotlin/MainActivity.kt
@@ -3,10 +3,7 @@ package com.aleangelozi.mysamplechatappkotlin
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import zendesk.chat.Chat
-import zendesk.chat.ChatConfiguration
-import zendesk.chat.ChatEngine
-import zendesk.chat.PreChatFormFieldStatus
+import zendesk.chat.*
 import zendesk.messaging.MessagingActivity
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +11,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Chat.INSTANCE.init(this, "jkkJEDT18TAduH0eOJ2fL422Etj1IJxR")
+        Chat.INSTANCE.init(this, "jkkJEDT18TAduH0eOJ2fL422Etj1IJxR",
+            "a0ec348c439fef74d030281ba454857d3f4583c8d2756064")
 
         val mChatButton = findViewById<Button>(R.id.chat_button)
 
@@ -24,6 +22,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initializeChatSdk() {
+
+        val profileProvider = Chat.INSTANCE.providers()?.profileProvider()
+
+        val visitorInfo = VisitorInfo.builder()
+            .withName("Renato")
+            .build()
+
+        profileProvider?.setVisitorInfo(visitorInfo,null)
+
+
         val chatConfiguration = ChatConfiguration.builder()
                 .withPreChatFormEnabled(true)
                 .withTranscriptEnabled(false)
@@ -33,8 +41,10 @@ class MainActivity : AppCompatActivity() {
                 .withDepartmentFieldStatus(PreChatFormFieldStatus.HIDDEN)
                 .build()
 
+
         MessagingActivity.builder()
                 .withEngines(ChatEngine.engine())
                 .show(this,chatConfiguration)
     }
+
 }
