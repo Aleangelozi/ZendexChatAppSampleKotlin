f3ef9d41ff08fdfa84bc2fa908719fcba0fd2d3e
aleangelozi <aleangelozi@gmail.com>
2021-06-06 20:35:03 -0300

    Adding VisitorInfo observer

--- app/src/main/java/com/aleangelozi/mysamplechatappkotlin/MainActivity.kt
+++ app/src/main/java/com/aleangelozi/mysamplechatappkotlin/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import zendesk.chat.*
 import zendesk.messaging.MessagingActivity
 
+
 private const val TAG = "ActivityOverrides"
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +21,7 @@ class MainActivity : AppCompatActivity() {
         val mChatButton = findViewById<Button>(R.id.chat_button)
 
         mChatButton.setOnClickListener {
+
             initializeChatSdk()
         }
 
@@ -34,7 +36,26 @@ class MainActivity : AppCompatActivity() {
 
     private fun initializeChatSdk() {
 
-        val name = "Alessandro Angelozi Ribeiro"
+        initializeVisitorInfo()
+
+        val chatConfiguration = ChatConfiguration.builder()
+                .withPreChatFormEnabled(true)
+                .withTranscriptEnabled(false)
+                .withNameFieldStatus(PreChatFormFieldStatus.HIDDEN)
+                .withEmailFieldStatus(PreChatFormFieldStatus.HIDDEN)
+                .withPhoneFieldStatus(PreChatFormFieldStatus.HIDDEN)
+                .withDepartmentFieldStatus(PreChatFormFieldStatus.HIDDEN)
+                .build()
+
+        MessagingActivity.builder()
+                .withEngines(ChatEngine.engine())
+                .show(this,chatConfiguration)
+    }
+
+    private fun initializeVisitorInfo() {
+        var visitorSet = false
+
+        val name = "Sofia Angelozi Ribeiro"
 
         val newName = name.substringBefore(' ')
 
@@ -48,19 +69,40 @@ class MainActivity : AppCompatActivity() {
         profileProvider?.setVisitorInfo(visitorInfo,null)
 
 
-        val chatConfiguration = ChatConfiguration.builder()
-                .withPreChatFormEnabled(true)
-                .withTranscriptEnabled(false)
-                .withNameFieldStatus(PreChatFormFieldStatus.HIDDEN)
-                .withEmailFieldStatus(PreChatFormFieldStatus.HIDDEN)
-                .withPhoneFieldStatus(PreChatFormFieldStatus.HIDDEN)
-                .withDepartmentFieldStatus(PreChatFormFieldStatus.HIDDEN)
-                .build()
 
+        fun setupObserver() {
+            val observationScope = ObservationScope()
+            Chat.INSTANCE.providers()!!.chatProvider()
+                .observeChatState(observationScope, object : Observer<ChatState?> {
+                    override fun update(t: ChatState?) {
+                        val chatStatus = t?.chatSessionStatus
+                        // Status achieved after the PreChatForm is completed
+                        if (chatStatus == ChatSessionStatus.STARTED) {
+                            // Update the information MID chat here. All info but Department can be updated
+                            if (!visitorSet) {
+                                // Add here the code to set the visitor info - visitorInfo would be a VisitorInfo type variable containing all the information to set
+                                val visitorInfo = VisitorInfo.builder()
+                                    .withName(newName)
+                                    .withEmail("whatever@whatever.com")
+                                    .build()
+                                profileProvider?.setVisitorInfo(visitorInfo, null)
+                                visitorSet = true
+                            }
+                        } else {
+                            // There are few other statuses that you can observe but they are unused in this example
+                            Log.d(
+                                "DEBUG",
+                                "[observerSetup] - ChatSessionUpdate -> (unused) status : $chatStatus"
+                            )
+                        }
+                    }
+                })
+        }
 
-        MessagingActivity.builder()
-                .withEngines(ChatEngine.engine())
-                .show(this,chatConfiguration)
+        setupObserver()
+
     }
+
+
 
 }
